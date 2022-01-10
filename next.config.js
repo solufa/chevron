664e6c663ebb5cebab63f7114b2fe9fba8cdcc1a
solufa <solufa2020@gmail.com>
2022-01-11 07:44:25 +0900

    chore: remove assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: process.env.GITHUB_REPOSITORY
-    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}`
-    : '',
 }
